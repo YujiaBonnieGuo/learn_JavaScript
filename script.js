90b894b4cd670319f744ed8bc87640fe170bbae5
Bonnie Guo <guoyuji@americas.manulife.net>
2019-09-11 16:00:05 -0400

    add set function

--- script.js
+++ script.js
@@ -21,5 +21,8 @@ m.has('Adam'); // 是否存在key 'Adam': true
 m.get('Adam'); // 67
 m.delete('Adam'); // 删除key 'Adam'
 m.get('Adam'); // undefined
+
 var s1 = new Set(); // 空Set
 var s2 = new Set([1, 2, 3]); // 含1, 2, 3
+var s = new Set([1, 2, 3, 3, '3']);
+s; // Set {1, 2, 3, "3"}

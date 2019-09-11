46acc64e2f8205bdda768f46adf0e6425eaeac84
Bonnie Guo <guoyuji@americas.manulife.net>
2019-09-11 15:58:28 -0400

    add map function

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ var person={
 };
 console.log(person);
 
+var m = new Map([['Michael', 95], ['Bob', 75], ['Tracy', 85]]);
+var m = new Map(); // 空Map
+m.set('Adam', 67); // 添加新的key-value
+m.set('Bob', 59);
+m.has('Adam'); // 是否存在key 'Adam': true
+m.get('Adam'); // 67
+m.delete('Adam'); // 删除key 'Adam'
+m.get('Adam'); // undefined

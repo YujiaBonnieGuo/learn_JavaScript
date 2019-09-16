e9a3eddb654122da97a1fdb70895d30980e3bccf
Bonnie Guo <guoyuji@americas.manulife.net>
2019-09-16 14:13:13 -0400

    sorting the string

--- script.js
+++ script.js
@@ -131,3 +131,16 @@ var xiaoming = {
 
 xiaoming.age; // function xiaoming.age()
 xiaoming.age(); // 今年调用是25,明年调用就变成26了
+
+var arr = ['Google', 'apple', 'Microsoft'];
+arr.sort(function (s1, s2) {
+    x1 = s1.toUpperCase();
+    x2 = s2.toUpperCase();
+    if (x1 < x2) {
+        return -1;
+    }
+    if (x1 > x2) {
+        return 1;
+    }
+    return 0;
+});

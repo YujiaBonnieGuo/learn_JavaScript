ab81b4d83b56439c4fbf66b689343f769fa771d7
Bonnie Guo <guoyuji@americas.manulife.net>
2019-09-24 15:03:50 -0400

    Hello, Michael!

--- hello.js
+++ hello.js
@@ -1,3 +1,12 @@
-'use strict';
+function hello() {
+    console.log('Hello, world!');
+}
 
-console.log('Hello, world! ');
+function greet(name) {
+    console.log('Hello, ' + name + '!');
+}
+
+module.exports = {
+    hello: hello,
+    greet: greet
+};

0e2058e1559f13c2164c60f6fe241c0fcaaeb53e
Bonnie Guo <guoyuji@americas.manulife.net>
2019-09-24 15:05:16 -0400

    Hello, Michael!

--- hello.js
+++ hello.js
@@ -1,16 +1,18 @@
-// hello.js
-
-function hello() {
-    console.log('Hello, world!');
-}
-
-function greet(name) {
-    console.log('Hello, ' + name + '!');
-}
-
-function hello() {
-    console.log('Hello, world!');
-}
-
-exports.hello = hello;
-exports.greet = greet;
+// 准备module对象:
+var module = {
+    id: 'hello',
+    exports: {}
+};
+var load = function (module) {
+    // 读取的hello.js代码:
+    function greet(name) {
+        console.log('Hello, ' + name + '!');
+    }
+    
+    module.exports = greet;
+    // hello.js代码结束
+    return module.exports;
+};
+var exported = load(module);
+// 保存module:
+save(module, exported);
